29ef34260f132d24f8571380ca9ea26dbd1f21cf
rudrabhikadiya3 <rudra.rnw@gmail.com>
2022-11-24 12:25:03 +0530

    24/11/22

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:bmi_calculator/BmiCalculator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => calc(),
+      },
+    ),
+  );
+}
